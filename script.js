a0274b28117c01d46607a6f22ee4e70836eff698
Suraj Bhandari <94055207+SurajBhandari5110@users.noreply.github.com>
2022-09-25 10:28:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+document.getElementById('input').addEventListener('change', () => {
+
+if (document.body.className.indexOf('dark') == -1) {
+    document.body.classList.add('dark')
+}
+    else {
+        document.body.classList.remove('dark')
+    }
+
+})
